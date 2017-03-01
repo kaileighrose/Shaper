ef1eb362a9847d3c24a3f665732ed2d389f045ba
kaileighrose <kaileighrose@gmail.com>
2017-03-01 14:03:59 -0500

    add canvas to boards#show and build out some canvas details within JS canvas function

--- app/assets/javascripts/canvas.js
+++ app/assets/javascripts/canvas.js
@@ -1,3 +1,8 @@
-function Canvas() {
+var canvas = document.getElementById('canvas');
+function Canvas(canvas) {
+  this.canvas = canvas;
+  this.width = window.innerWidth;
+  this.height = window.innerHeight;
+  this.ctx = canvas.getContext('2d');
   
 }
